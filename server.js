8ab06323c41b1f44c90cc474ca52e178a752f3b2
Ed Summers <ehs@pobox.com>
2011-12-19 02:55:00 -0500

    use a list of urls to keep track of what has been seen before

--- server.js
+++ server.js
@@ -4,15 +4,33 @@ var express = require("express");
 
 var nytimes_key = process.env.NYTIMES_KEY
 var server = express.createServer();
+var poll_interval = 1000 * 60 * 10;
+var seen = []
 var streams = [];
-var poll_interval = 10000;
-var last_seen = "";
 
 function home(req, res) {
   nytimes(function(json) {
     res.contentType('application/json');
     res.send(json);
   });
+}
+
+function poll() {
+  nytimes(function(json) {
+    var update = JSON.parse(json);
+    _.each(update.results.reverse(), function(i) {
+      if (! _.include(seen, i.url)) {
+        console.log("found " + i.headline);
+        seen.push(i.url);
+        // to avoid memory bloat, only remember last 1000 urls
+        seen = _.last(seen, 1000);
+      }
+    });
+  });
+  setTimeout(poll, poll_interval);
+};
+
+function publish() {
 }
 
 function nytimes(f) {
@@ -26,23 +44,12 @@ function nytimes(f) {
       json += chunk;
     });
     res.on('end', function() {
-      f(json);
+      if (res.statusCode == 200) f(json);
     });
+  }).on('error', function(e) {
+    console.log("error: " + e);
   });
 }
-
-function poll() {
-  nytimes(function(json) {
-    var update = JSON.parse(json);
-    _.each(update.results.reverse(), function(i) {
-      if (i.created >= last_seen) {
-        console.log("[" + i.created + " > " + last_seen + "] " + i.headline);
-        last_seen = i.created;
-      }
-    });
-  });
-  setTimeout(poll, poll_interval);
-};
 
 poll();
 server.get('/', home);
